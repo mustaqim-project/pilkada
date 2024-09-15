b2f42afb85a7d5d2a29390c2cb2ae1fa2c850b85
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-15 23:25:35 +0700

    sdfsdfsdf

--- resources/views/desktop/layouts/app.blade.php
+++ resources/views/desktop/layouts/app.blade.php
@@ -30,7 +30,7 @@
 
             <!-- Page Content -->
             <main>
-                @yield("content")
+                {{-- @yield("content") --}}
                 {{ $slot }}
             </main>
         </div>
