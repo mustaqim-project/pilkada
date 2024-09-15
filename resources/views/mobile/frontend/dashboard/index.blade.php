29c8a40af0a2285a5fef8bf9c30342ad6f134669
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-15 15:10:23 +0700

    asdfasdf

--- resources/views/mobile/frontend/dashboard/index.blade.php
+++ resources/views/mobile/frontend/dashboard/index.blade.php
@@ -26,10 +26,9 @@
             <div class="card card-style ml-0 mr-0 bg-white">
                 <div class="row mt-3 pt-1 mb-3">
                     <div class="col-4 text-center">
-                        <a href="kunjungan-warga.html">
+                        <a href="{{ route('kanvasing.create') }}">
                             <i class="ml-3 mr-3" data-feather="user" style="color: #FF5733;"></i>
-                            <h5 class="color-black font-13 font-500 line-height-s" style="font-size: 0.8125rem;">Kunjungan
-                                Warga</h5>
+                            <h5 class="color-black font-13 font-500 line-height-s" style="font-size: 0.8125rem;">Kunjungan Warga</h5>
                         </a>
                     </div>
                     <div class="col-4 text-center">
