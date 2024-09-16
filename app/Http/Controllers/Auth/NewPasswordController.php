e40d3655f659353a5d44cf65511ce8d6b11b2291
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-16 12:24:28 +0700

    uiopuuirteythjk

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -32,8 +32,11 @@ class NewPasswordController extends Controller
             return view('auth.reset-password', ['request' => $request]);
         }
     }
-
-
+    /**
+     * Handle an incoming new password request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
@@ -42,6 +45,9 @@ class NewPasswordController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
         $status = Password::reset(
             $request->only('email', 'password', 'password_confirmation', 'token'),
             function ($user) use ($request) {
@@ -54,18 +60,12 @@ class NewPasswordController extends Controller
             }
         );
 
-        $detect = new MobileDetect();
-
-        if ($status == Password::PASSWORD_RESET) {
-            if ($detect->isMobile() || $detect->isTablet()) {
-                return view('mobile.auth.login');
-            } else {
-                return view('desktop.auth.login');
-            }
-        } else {
-            return back()->withInput($request->only('email'))
-                         ->withErrors(['email' => __($status)]);
-        }
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        return $status == Password::PASSWORD_RESET
+                    ? redirect()->route('login')->with('status', __($status))
+                    : back()->withInput($request->only('email'))
+                            ->withErrors(['email' => __($status)]);
     }
-
 }
