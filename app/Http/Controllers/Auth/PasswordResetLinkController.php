a21750cc647fa788e365f87a5ccfd7b20d00eb0f
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-16 12:11:24 +0700

    asdfasdfasdf

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -11,14 +11,15 @@ use Detection\MobileDetect;
 
 class PasswordResetLinkController extends Controller
 {
-    /**
-     * Display the password reset link request view.
-     */
+
     public function create(): View
     {
+
         $detect = new MobileDetect();
 
-        if ($detect->isMobile() || $detect->isTablet()) {
+        if ($detect->isMobile()) {
+            return view('mobile.auth.forgot-password');
+        } elseif ($detect->isTablet()) {
             return view('mobile.auth.forgot-password');
         } else {
             return view('auth.forgot-password');
@@ -36,19 +37,14 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
         $status = Password::sendResetLink(
             $request->only('email')
         );
 
-        // Check if the reset link was sent successfully
-        if ($status == Password::RESET_LINK_SENT) {
-            return back()->with('status', __($status));
-        } else {
-            return back()->withInput($request->only('email'))
-                         ->withErrors(['email' => __($status)]);
-        }
+        return $status == Password::RESET_LINK_SENT
+                    ? back()->with('status', __($status))
+                    : back()->withInput($request->only('email'))
+                            ->withErrors(['email' => __($status)]);
+
     }
 }
