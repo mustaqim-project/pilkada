6e80b6c09ff02121dcc8b6efc3f28a13f4e77eab
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-17 00:05:53 +0700

    dsfgsdfgsdfg

--- resources/views/mobile/frontend/layout/master.blade.php
+++ resources/views/mobile/frontend/layout/master.blade.php
@@ -55,28 +55,26 @@
 
         <!-- Be sure this is on your main visiting page, for example, the index page -->
         <!-- Install Prompt for Android -->
-        <div id="menu-install-pwa-android" class="menu menu-box-bottom menu-box-detached rounded-l" data-menu-height="350" data-menu-effect="menu-parallax">
+        {{-- <div id="menu-install-pwa-android" class="menu menu-box-bottom menu-box-detached rounded-l" data-menu-height="350" data-menu-effect="menu-parallax">
             <div class="boxed-text-l mt-4">
                 <img class="rounded-l mb-3" src="{{ asset('mobile/images/logo.png') }}" alt="img" width="90">
                 <h4 class="mt-3">Sidaksis on your Home Screen</h4>
                 <p>Install Sidaksis on your home screen, and access it just like a regular app. It really is that simple!</p>
                 <a href="#" class="pwa-install btn btn-s rounded-s shadow-l text-uppercase font-900 bg-highlight mb-2">Add to Home Screen</a><br>
-                {{-- <a href="#" class="pwa-dismiss close-menu color-gray2-light text-uppercase font-900 opacity-60 font-10">Maybe later</a> --}}
                 <div class="clear"></div>
             </div>
-        </div>
+        </div> --}}
 
         <!-- Install instructions for iOS -->
-        <div id="menu-install-pwa-ios" class="menu menu-box-bottom menu-box-detached rounded-l" data-menu-height="320" data-menu-effect="menu-parallax">
+        {{-- <div id="menu-install-pwa-ios" class="menu menu-box-bottom menu-box-detached rounded-l" data-menu-height="320" data-menu-effect="menu-parallax">
             <div class="boxed-text-xl mt-4">
                 <img class="rounded-l mb-3" src="{{ asset('mobile/images/logo.png') }}" alt="img" width="90">
                 <h4 class="mt-3">Sidaksis on your Home Screen</h4>
                 <p class="mb-0 pb-3">Install Sidaksis on your home screen, and access it just like a regular app. Open your Safari menu and tap "Add to Home Screen".</p>
                 <div class="clear"></div>
-                {{-- <a href="#" class="pwa-dismiss close-menu color-highlight font-800 opacity-80 text-center text-uppercase">Maybe later</a><br> --}}
                 <i class="fa-ios-arrow fa fa-caret-down font-40"></i>
             </div>
-        </div>
+        </div> --}}
     </div>
 
     <script type="text/javascript" src="{{ asset('mobile/scripts/jquery.js') }}"></script>
