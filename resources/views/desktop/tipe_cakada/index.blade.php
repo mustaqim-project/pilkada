a451fb6bb0737bddc1f3400145eb780dec172ab4
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-17 13:20:31 +0700

    asjdfhajksdfhasjkdf

--- resources/views/desktop/tipe_cakada/index.blade.php
+++ resources/views/desktop/tipe_cakada/index.blade.php
@@ -11,12 +11,12 @@
                 <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0"> Dashboard </h1>
                 <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
                     <li class="breadcrumb-item text-muted">
-                        <a href="../../demo1/dist/index.html" class="text-muted text-hover-primary">Home</a>
+                        <a href="{{ route('dashboard') }}" class="text-muted text-hover-primary">Home</a>
                     </li>
                     <li class="breadcrumb-item">
                         <span class="bullet bg-gray-400 w-5px h-2px"></span>
                     </li>
-                    <li class="breadcrumb-item text-muted"> Dashboards </li>
+                    <li class="breadcrumb-item text-muted"> Tipe Pilkada </li>
                 </ul>
             </div>
         </div>
