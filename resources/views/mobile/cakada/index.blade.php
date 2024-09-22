a21d0daa50feaec246c6432b0c572c9462eed4db
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-22 09:40:43 +0700

    asdfasdfasd43563456

--- resources/views/mobile/cakada/index.blade.php
+++ resources/views/mobile/cakada/index.blade.php
@@ -53,62 +53,88 @@
     </script>
     @endif
 
-    <div class="card card-style">
-        <div class="content mb-2">
-            <h3>Calon Kelapa Daerah</h3>
-            <a href="{{ route('cakada.create') }}" class="btn btn-primary">
-                <i class="fas fa-plus"></i> {{ __('Tambah Data') }}
-            </a>
-            <!-- Add table-responsive to make table scrollable on small screens -->
-            <div class="table-responsive">
-                <table class="table text-center rounded-sm shadow-l">
-                    <thead>
-                        <tr class="bg-gray1-dark">
-                            <th scope="col" class="color-theme">#</th>
-                            <th scope="col" class="color-theme">Provinsi</th>
-                            <th scope="col" class="color-theme">Kabupaten/Kota</th>
-                            <th scope="col" class="color-theme">Nama Calon Kepala</th>
-                            <th scope="col" class="color-theme">Nama Calon Wakil</th>
-                            <th scope="col" class="color-theme">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php
-                            $provinsiMap = collect($provinsi)->pluck('name', 'id')->toArray();
-                        @endphp
+    <div class="content mb-2">
+        <h3>Calon Kelapa Daerah</h3>
+        <a href="{{ route('cakada.create') }}" class="btn btn-primary">
+            <i class="fas fa-plus"></i> {{ __('Tambah Data') }}
+        </a>
+        <div class="table-responsive">
+            <table class="table text-center rounded-sm shadow-l">
+                <thead>
+                    <tr class="bg-gray1-dark">
+                        <th scope="col" class="color-theme">#</th>
+                        <th scope="col" class="color-theme">Provinsi</th>
+                        <th scope="col" class="color-theme">Kabupaten/Kota</th>
+                        <th scope="col" class="color-theme">Nama Calon Kepala</th>
+                        <th scope="col" class="color-theme">Nama Calon Wakil</th>
+                        <th scope="col" class="color-theme">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php
+                        $provinsiMap = collect($provinsi)->pluck('name', 'id')->toArray();
+                    @endphp
 
-                        @forelse ($cakadas as $cakada)
-                            @php
-                                $provinsiName = $provinsiMap[$cakada->provinsi] ?? 'Unknown';
-                                $regencies = app('App\Http\Controllers\CakadaController')->getRegencies($cakada->provinsi);
-                                $regenciesMap = collect($regencies)->pluck('name', 'id')->toArray();
-                                $kabupatenKotaName = $regenciesMap[$cakada->kabupaten_kota] ?? 'Unknown';
-                            @endphp
-                            <tr>
-                                <td scope="row">{{ $cakada->id }}</td>
-                                <td class="color-dark">{{ $provinsiName }}</td>
-                                <td class="color-dark">{{ $kabupatenKotaName }}</td>
-                                <td class="color-dark">{{ $cakada->nama_calon_kepala }}</td>
-                                <td class="color-dark">{{ $cakada->nama_calon_wakil }}</td>
-                                <td class="color-dark">
-                                    <button class="btn btn-warning btn-sm btn-edit" action="{{ route('cakada.edit', $cakada->id) }}">Edit</button>
-                                    <form action="{{ route('cakada.destroy', $cakada->id) }}" method="POST" style="display:inline;">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="6" class="text-center">Tidak ada data.</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
-            </div>
+                    @forelse ($cakadas as $cakada)
+                        @php
+                            $provinsiName = $provinsiMap[$cakada->provinsi] ?? 'Unknown';
+                            $regencies = app('App\Http\Controllers\CakadaController')->getRegencies($cakada->provinsi);
+                            $regenciesMap = collect($regencies)->pluck('name', 'id')->toArray();
+                            $kabupatenKotaName = $regenciesMap[$cakada->kabupaten_kota] ?? 'Unknown';
+                        @endphp
+                        <tr>
+                            <td scope="row">{{ $cakada->id }}</td>
+                            <td class="color-dark">{{ $provinsiName }}</td>
+                            <td class="color-dark">{{ $kabupatenKotaName }}</td>
+                            <td class="color-dark">{{ $cakada->nama_calon_kepala }}</td>
+                            <td class="color-dark">{{ $cakada->nama_calon_wakil }}</td>
+                            <td class="color-dark">
+                                <button class="btn btn-warning btn-sm btn-edit" data-id="{{ $cakada->id }}">Edit</button>
+                                <form action="{{ route('cakada.destroy', $cakada->id) }}" method="POST" style="display:inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="6" class="text-center">Tidak ada data.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
     </div>
+
+    <script>
+        $(document).ready(function() {
+            $('.btn-edit').click(function() {
+                let id = $(this).data('id');
+                $.ajax({
+                    url: `/cakada/${id}/edit`,
+                    method: 'GET',
+                    success: function(data) {
+                        $('#modalCakada').modal('show');
+                        $('#cakada_id').val(data.id);
+                        $('#provinsi').val(data.provinsi);
+                        $('#tipe_cakada_id').val(data.tipe_cakada_id);
+                        $('#nama_calon_kepala').val(data.nama_calon_kepala);
+                        $('#nama_calon_wakil').val(data.nama_calon_wakil);
+                        $('#formCakada').attr('action', `/cakada/${id}`);
+                        $('#formCakada').append('<input type="hidden" name="_method" value="PUT">');
+
+                        // Trigger change to populate kabupaten_kota dropdown
+                        $('#provinsi').trigger('change');
+                        setTimeout(function() {
+                            $('#kabupaten_kota').val(data.kabupaten_kota);
+                        }, 500);
+                    }
+                });
+            });
+        });
+    </script>
+
 </div>
 
 <script>
