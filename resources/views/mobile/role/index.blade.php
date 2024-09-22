8ac6878e3149ee6bf5c226c92da963993c417fbd
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-22 08:23:16 +0700

    asdfas234234

--- resources/views/mobile/role/index.blade.php
+++ resources/views/mobile/role/index.blade.php
@@ -1,81 +1,79 @@
 @extends('mobile.frontend.layout.master')
 
 @section('content')
-    <section class="section">
-        <div class="section-header">
-            <h1>{{ __('Roles and Permission') }}</h1>
-        </div>
 
-        <div class="card card-primary">
-            <div class="card-header">
-                <h4>{{ __('Roles and Permission') }}</h4>
-                <div class="card-header-action">
-                    <a href="{{ route('role.create') }}" class="btn btn-primary">
-                        <i class="fas fa-plus"></i> {{ __('Create new') }}
-                    </a>
-                </div>
-            </div>
 
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-striped" id="table">
-                        <thead>
-                            <tr>
-                                <th class="text-center">
-                                    #
-                                </th>
-                                <th>{{ __('Role Name') }}</th>
-                                <th>{{ __('Permissions') }}</th>
-                                <th>{{ __('Action') }}</th>
+<div class="page-content">
+    <div class="page-title page-title-small">
+        <h2><a href="{{ route('dashboard') }}" data-back-button><i class="fa fa-arrow-left"></i></a>Beranda</h2>
+    </div>
+    <div class="card header-card shape-rounded" data-card-height="210">
+        <div class="card-overlay bg-highlight opacity-95"></div>
+        <div class="card-overlay dark-mode-tint"></div>
+        <div class="card-bg preload-img" data-src="admin/mobile/myhr/images/sikad.png"></div>
+    </div>
 
-                            </tr>
-                        </thead>
-                        <tbody>
+    <div class="card card-style">
+        <div class="content mb-2">
+            <h3>Role Permision</h3>
+            <a href="{{ route('role.create') }}" class="btn btn-primary">
+                <i class="fas fa-plus"></i> {{ __('Create new') }}
+            </a>
+            <table class="table table-borderless text-center rounded-sm shadow-l" style="overflow: hidden;">
+                <thead>
 
-                            @foreach ($roles as $role)
-                            <tr>
-                                <td>{{ $role->id }}</td>
-                                <td>{{ $role->name }}</td>
-                                <td>
-                                    @foreach ($role->permissions as $permission)
-                                        <span class="badge bg-primary text-light">{{ $permission->name }}</span>
-                                    @endforeach
-                                    @if ($role->name === 'Admin')
-                                    <span class="badge bg-danger text-light">{{ __('All Permissions') }} *</span>
-                                    @endif
-                                </td>
-                                <td>
-                                    @if ($role->name != 'Admin')
-                                    <a href="{{ route('role.edit', $role->id) }}"
-                                        class="btn btn-primary"><i class="fas fa-edit"></i></a>
+                    <tr class="bg-gray1-dark">
+                        <th scope="col" class="color-theme">#</th>
+                        <th scope="col" class="color-theme">Role Name</th>
+                        <th scope="col" class="color-theme">Permissions</th>
+                        <th scope="col" class="color-theme">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
 
-                                    <a href="{{ route('role.destory', $role->id) }}"
-                                        class="btn btn-danger delete-item"><i
-                                            class="fas fa-trash-alt"></i></a>
-                                    @endif
-                                </td>
-                            </tr>
+                    @foreach ($roles as $role)
+                    <tr>
+                        <td scope="row">{{ $role->id }}</td>
+                        <td class="color-green1-dark">{{ $role->name }}</td>
+                        <td class="color-green1-dark">
+                            @foreach ($role->permissions as $permission)
+                            <span class="badge bg-primary text-light">{{ $permission->name }}</span>
                             @endforeach
+                            @if ($role->name === 'Admin')
+                            <span class="badge bg-danger text-light">{{ __('All Permissions') }} *</span>
+                            @endif
+                        </td>
+                        <td class="color-green1-dark">
+                            @if ($role->name != 'Admin')
+                            <a href="{{ route('role.edit', $role->id) }}" class="btn btn-primary"><i class="fas fa-edit"></i></a>
 
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-
-
+                            <a href="{{ route('role.destory', $role->id) }}" class="btn btn-danger delete-item"><i class="fas fa-trash-alt"></i></a>
+                            @endif
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-    </section>
+    </div>
+
+
+
+
+
+
+</div>
+</section>
 @endsection
 
 @push('scripts')
-    <script>
+<script>
+    $("#table").dataTable({
+        "columnDefs": [{
+            "sortable": false
+            , "targets": [2, 3]
+        }]
+    });
 
-        $("#table").dataTable({
-            "columnDefs": [{
-                "sortable": false,
-                "targets": [2, 3]
-            }]
-        });
-
-    </script>
+</script>
 @endpush
