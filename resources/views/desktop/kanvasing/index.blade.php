57051e59d5cc7373c6158dc136a80433f973f371
Agung Mustaqim <agungmustaqim15@gmail.com>
2024-09-25 13:47:49 +0700

    dsasdas234324

--- resources/views/desktop/kanvasing/index.blade.php
+++ resources/views/desktop/kanvasing/index.blade.php
@@ -42,218 +42,128 @@
             </script>
             @endif
 
-            <div class="card card-style">
-                <div class="responsive-iframe add-iframe">
-                    <iframe class="location-map" src='https://maps.google.com/?ie=UTF8&amp;ll=47.595131,-122.330414&amp;spn=0.006186,0.016512&amp;t=h&amp;z=17&amp;output=embed'></iframe>
-                </div>
-            </div>
+
             <div id="map" style="display: block; height: 400px;"></div> <!-- Make sure the map is visible -->
 
-
-
-
-
-
-        <!-- Tabel Daftar Cakada -->
-        <div class="card mb-4">
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Provinsi</th>
-                                <th>Kabupaten/Kota</th>
-                                <th>Kecamatan</th>
-                                <th>Kelurahan</th>
-                                <th>Nama KK</th>
-                                <th>Nomor HP</th>
-                                <th>Alamat</th>
-                                <th>Elektabilitas</th>
-                                <th>Popularitas</th>
-                                <th>Jenis Kelamin</th>
-                                <th>Usia</th>
-                                <th>Jumlah Pemilih</th>
-                                <th>Alasan</th>
-                                <th>Pesan</th>
-                                <th>Deskripsi</th>
-                                <th>Foto</th>
-                                <th>Long</th>
-                                <th>Lat</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($kanvasings as $kanvasing)
-                            <tr>
-                                <td>{{ $kanvasing->id }}</td>
-                                <td>{{ $kanvasing->provinsi_name }}</td>
-                                <td>{{ $kanvasing->kabupaten_name }}</td>
-                                <td>{{ $kanvasing->kecamatan_name }}</td>
-                                <td>{{ $kanvasing->kelurahan_name }}</td>
-                                <td>{{ $kanvasing->nama_kk }}</td>
-                                <td>{{ $kanvasing->nomor_hp }}</td>
-                                <td>{{ $kanvasing->alamat }}</td>
-                                <!-- Translating elektabilitas -->
-                                <td>
-                                    @switch($kanvasing->elektabilitas)
-                                    @case(1)
-                                    Memilih
-                                    @break
-                                    @case(2)
-                                    Tidak Memilih
-                                    @break
-                                    @case(3)
-                                    Ragu-Ragu
-                                    @break
-                                    @default
-                                    Tidak Diketahui
-                                    @endswitch
-                                </td>
-
-                                <!-- Translating popularitas -->
-                                <td>
-                                    @switch($kanvasing->popularitas)
-                                    @case(1)
-                                    Kenal
-                                    @break
-                                    @case(2)
-                                    Tidak Kenal
-                                    @break
-                                    @default
-                                    Tidak Diketahui
-                                    @endswitch
-                                </td>
-
-                                <!-- Translating jenis_kelamin -->
-                                <td>
-                                    @switch($kanvasing->jenis_kelamin)
-                                    @case(1)
-                                    Laki-laki
-                                    @break
-                                    @case(2)
-                                    Perempuan
-                                    @break
-                                    @default
-                                    Tidak Diketahui
-                                    @endswitch
-                                </td>
-                                <td>{{ $kanvasing->usia }}</td>
-                                <td>{{ $kanvasing->jum_pemilih }}</td>
-                                <td>{{ $kanvasing->alasan }}</td>
-                                <td>{{ $kanvasing->pesan }}</td>
-                                <td>{{ $kanvasing->deskripsi }}</td>
-                                <td>
-                                    <img src="{{ asset($kanvasing->foto) }}" alt="Foto" style="width: 50px; height: auto;">
-                                </td>
-                                <td>{{ $kanvasing->lang }}</td>
-                                <td>{{ $kanvasing->lat }}</td>
-                                <td>
-                                    <div class="btn-group" role="group" aria-label="Basic example">
-                                        <a href="{{ route('kanvasing.edit', $kanvasing->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                                        <form action="{{ route('kanvasing.destroy', $kanvasing->id) }}" method="POST" style="display:inline;">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
-                            @empty
-                            <tr>
-                                <td colspan="20" class="text-center">Tidak ada data.</td>
-                            </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
+            <!-- Tabel Daftar Cakada -->
+            <div class="card mb-4">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Provinsi</th>
+                                    <th>Kabupaten/Kota</th>
+                                    <th>Kecamatan</th>
+                                    <th>Kelurahan</th>
+                                    <th>Nama KK</th>
+                                    <th>Nomor HP</th>
+                                    <th>Alamat</th>
+                                    <th>Elektabilitas</th>
+                                    <th>Popularitas</th>
+                                    <th>Jenis Kelamin</th>
+                                    <th>Usia</th>
+                                    <th>Jumlah Pemilih</th>
+                                    <th>Alasan</th>
+                                    <th>Pesan</th>
+                                    <th>Deskripsi</th>
+                                    <th>Foto</th>
+                                    <th>Long</th>
+                                    <th>Lat</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($kanvasings as $kanvasing)
+                                <tr>
+                                    <td>{{ $kanvasing->id }}</td>
+                                    <td>{{ $kanvasing->provinsi_name }}</td>
+                                    <td>{{ $kanvasing->kabupaten_name }}</td>
+                                    <td>{{ $kanvasing->kecamatan_name }}</td>
+                                    <td>{{ $kanvasing->kelurahan_name }}</td>
+                                    <td>{{ $kanvasing->nama_kk }}</td>
+                                    <td>{{ $kanvasing->nomor_hp }}</td>
+                                    <td>{{ $kanvasing->alamat }}</td>
+                                    <td>{{ $kanvasing->elektabilitas == 1 ? 'Memilih' : ($kanvasing->elektabilitas == 2 ? 'Tidak Memilih' : 'Tidak Diketahui') }}</td>
+                                    <td>{{ $kanvasing->popularitas == 1 ? 'Kenal' : ($kanvasing->popularitas == 2 ? 'Tidak Kenal' : 'Tidak Diketahui') }}</td>
+                                    <td>{{ $kanvasing->jenis_kelamin == 1 ? 'Laki-laki' : ($kanvasing->jenis_kelamin == 2 ? 'Perempuan' : 'Tidak Diketahui') }}</td>
+                                    <td>{{ $kanvasing->usia }}</td>
+                                    <td>{{ $kanvasing->jum_pemilih }}</td>
+                                    <td>{{ $kanvasing->alasan }}</td>
+                                    <td>{{ $kanvasing->pesan }}</td>
+                                    <td>{{ $kanvasing->deskripsi }}</td>
+                                    <td>
+                                        <img src="{{ asset($kanvasing->foto) }}" alt="Foto" style="width: 50px; height: auto;">
+                                    </td>
+                                    <td>{{ $kanvasing->lang }}</td>
+                                    <td>{{ $kanvasing->lat }}</td>
+                                    <td>
+                                        <div class="btn-group" role="group" aria-label="Basic example">
+                                            <a href="{{ route('kanvasing.edit', $kanvasing->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                            <form action="{{ route('kanvasing.destroy', $kanvasing->id) }}" method="POST" style="display:inline;">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @empty
+                                <tr>
+                                    <td colspan="20" class="text-center">Tidak ada data.</td>
+                                </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
+
+
         </div>
-
     </div>
-</div>
+    </div>
 
 
-<script>
-    $(document).ready(function() {
-        checkLocationPermission();
-
-        // Extract latitude and longitude from the kanvasings data
-        var locations = @json($kanvasings->map(function($kanvasing) {
-            return ['lat' => $kanvasing->lat, 'lng' => $kanvasing->lang];
-        }));
-
-        // Initialize the map
-        var map = L.map('map').setView([locations[0].lat, locations[0].lng], 13); // Start at the first location
-
-        // Add a tile layer (OpenStreetMap in this case)
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19
-        }).addTo(map);
-
-        // Loop through the locations and add markers to the map
-        locations.forEach(function(location) {
-            L.marker([location.lat, location.lng]).addTo(map)
-                .bindPopup('Lat: ' + location.lat + ', Lng: ' + location.lng)
-                .openPopup();
-        });
-
-        $('.get-location').on('click', function(e) {
-            e.preventDefault(); // Prevent the default anchor click behavior
-
-            // Run the function to get the user's location
-            getLocation();
-        });
-
-        // Function to get the user's location and update the map
-        function getLocation() {
+    <script>
+        $(document).ready(function() {
+            // Extract latitude and longitude from the kanvasings data
+            var locations = @json($kanvasings->map(function($kanvasing) {
+                return ['lat' => $kanvasing->lat, 'lng' => $kanvasing->lang];
+            }));
+    
+            // Initialize the map
+            var map = L.map('map').setView([locations[0].lat, locations[0].lng], 13); // Start at the first location
+    
+            // Add a tile layer (OpenStreetMap in this case)
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19
+            }).addTo(map);
+    
+            // Loop through the locations and add markers to the map
+            locations.forEach(function(location) {
+                L.marker([location.lat, location.lng]).addTo(map)
+                    .bindPopup('Lat: ' + location.lat + ', Lng: ' + location.lng)
+                    .openPopup();
+            });
+    
+            // Optionally, handle user location
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition, showError);
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var userLat = position.coords.latitude;
+                    var userLng = position.coords.longitude;
+    
+                    // Center the map on user location
+                    map.setView([userLat, userLng], 13);
+                    L.marker([userLat, userLng]).addTo(map)
+                        .bindPopup('Your Location')
+                        .openPopup();
+                });
             } else {
                 console.log("Geolocation is not supported by this browser.");
             }
-        }
-
-        function showPosition(position) {
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
-
-            // Update hidden fields with user's location
-            $('#lat').val(lat);
-            $('#lang').val(lng);
-
-            // Center the map and add a marker for the user's location
-            map.setView([lat, lng], 13);
-            L.marker([lat, lng]).addTo(map)
-                .bindPopup('Lokasi Saya')
-                .openPopup();
-
-            // Get the location name using reverse geocoding
-            fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`)
-                .then(response => response.json())
-                .then(data => {
-                    var locationName = data.display_name;
-                    $('#location_name').val(locationName);
-                })
-                .catch(error => console.error('Error fetching location name:', error));
-        }
-
-        function showError(error) {
-            switch (error.code) {
-                case error.PERMISSION_DENIED:
-                    console.log("User denied the request for Geolocation.");
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    console.log("Location information is unavailable.");
-                    break;
-                case error.TIMEOUT:
-                    console.log("The request to get user location timed out.");
-                    break;
-                case error.UNKNOWN_ERROR:
-                    console.log("An unknown error occurred.");
-                    break;
-            }
-        }
-    });
-</script>
-@endsection
+        });
+    </script>
+    
+    @endsection
--- resources/views/desktop/layouts/master.blade.php
+++ resources/views/desktop/layouts/master.blade.php
@@ -14,6 +14,9 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <!-- Scripts -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+
     <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 
